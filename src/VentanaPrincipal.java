696db6de96274ce56e8ce1b281622ef6d77b1637
mell9413 <mell9413@hotmail.com>
2014-09-20 11:56:17 -0600

    Se modifican las dimensiones de la ventana principal

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -20,8 +20,8 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         initComponents();
         getContentPane().setBackground(new java.awt.Color(229,55,55));
         Nombre.setBackground(Color.white);
-        //Nombre.setOpaque(true);
-        //setResizable(false);
+        Nombre.setOpaque(true);
+        setResizable(false);
     }
 
     /**
@@ -49,7 +49,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         setTitle("BAC San Jose");
         setBackground(new java.awt.Color(242, 36, 36));
         setFocusCycleRoot(false);
-        //setPreferredSize(new java.awt.Dimension(567, 380));
+        setPreferredSize(new java.awt.Dimension(567, 380));
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -68,9 +68,10 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(Logo)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(43, Short.MAX_VALUE)
                 .addComponent(Nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 625, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 52, Short.MAX_VALUE))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -96,6 +97,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
             }
         });
 
+        EtiquetaCantidadDeCajas.setForeground(new java.awt.Color(254, 254, 254));
         EtiquetaCantidadDeCajas.setText("Cantidad de Cajas:");
 
         CantidadCajas.setModel(new javax.swing.SpinnerNumberModel(Integer.valueOf(0), Integer.valueOf(0), null, Integer.valueOf(1)));
@@ -147,30 +149,28 @@ public class VentanaPrincipal extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(BtncambiarLogo)
+                .addGap(52, 52, 52)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(BtnCambiarNombre)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(41, 41, 41)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(52, 52, 52)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(BtnCambiarNombre)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(CantidadCajas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(EtiquetaCantidadDeCajas))
                         .addGap(50, 50, 50))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(BtnPredeterminado)
-                            .addComponent(BtnAceptar))))
-                .addGap(18, 18, 18))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(BtnPredeterminado)
+                        .addComponent(BtnAceptar)))
+                .addGap(18, 18, Short.MAX_VALUE))
         );
 
         CantidadCajas.getAccessibleContext().setAccessibleParent(CantidadCajas);
 
-        pack();
+        setBounds(0, 0, 690, 373);
     }// </editor-fold>//GEN-END:initComponents
 
     private void BtnPredeterminadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnPredeterminadoActionPerformed
