8395cafa8b0aa1f1c0944ca2095c12a7c4d72749
chef19 <teckevin_16@hotmail.com>
2014-10-02 15:36:51 -0600

    Dato de Hora

--- src/LeerTxt.java
+++ src/LeerTxt.java
@@ -14,7 +14,6 @@ import java.io.FileReader;
  */
 public class LeerTxt {
       int contador;
-      String detener = null;
       String comparar;
       int leer=3;
       int nueve=0;
@@ -24,7 +23,9 @@ public class LeerTxt {
       int dos=0;
       int tres=0;
       int cuatro=0;
-    public String Horas(String categoria) {
+      int a;
+      String categoria;
+    public String Horas() {
       String nl = System.getProperty("line.separator");
       File archivo = null;
       FileReader fr = null;
@@ -33,54 +34,63 @@ public class LeerTxt {
       try {
          // Apertura del fichero y creacion de BufferedReader para poder
          // hacer una lectura comoda (disponer del metodo readLine()).
-         archivo = new File ("Clientes/"+categoria+".txt");
-         fr = new FileReader (archivo);
-         br = new BufferedReader(fr);
+         
  
          // Lectura del fichero
          String linea;
          contador =0;
-         while((linea=br.readLine())!=null)
-             if (leer == contador){
-                 
-                 comparar = linea.substring(0, 2);
-                 //System.out.print(linea);
-                 if ("09".equals(comparar)){
-                     nueve += 1;
-                     System.out.print("nueve  "+ nueve+nl);
-                 }
-                 if ("10".equals(comparar)){
-                     diez += 1;
-                     System.out.print(diez);
-                }
-                 if ("11".equals(comparar)){
-                     once += 1;
-                     System.out.print(once);
-                }
-                 if ("01".equals(comparar)){
-                     una += 1;
-                     System.out.print(una);
-                }
-                 if ("02".equals(comparar)){
-                     dos += 1;
-                     System.out.print(dos);
-                }
-                 if ("03".equals(comparar)){
-                     tres += 1;
-                     System.out.print("tres  "+tres);
-                }
-                 if ("04".equals(comparar)){
-                     cuatro += 1;
-                     System.out.print(cuatro);
-                }
-                 contador +=1;
-                 leer = leer + 5;
-                 //linea = "";
+         
+         for (a=0;a<2;a++){
+             if (a==0){
+                 categoria="Embarazadas";
              }
-             else{
-                 contador += 1;
+             if (a==1){
+                 categoria="Regulares";
              }
-        
+            archivo = new File ("Clientes/"+categoria+".txt");
+            fr = new FileReader (archivo);
+            br = new BufferedReader(fr);
+            while((linea=br.readLine())!=null)
+                if (leer == contador){
+                    comparar = linea.substring(0, 2);
+                    if ("09".equals(comparar)){
+                        nueve += 1;
+                        System.out.println("nueve  "+ nueve);
+                    }
+                    if ("10".equals(comparar)){
+                        diez += 1;
+                        System.out.print(diez);
+                   }
+                    if ("11".equals(comparar)){
+                        once += 1;
+                        System.out.print(once);
+                   }
+                    if ("01".equals(comparar)){
+                        una += 1;
+                        System.out.print(una);
+                   }
+                    if ("02".equals(comparar)){
+                        dos += 1;
+                        System.out.println(dos);
+                   }
+                    if ("03".equals(comparar)){
+                        tres += 1;
+                        System.out.println("tres  "+tres);
+                   }
+                    if ("04".equals(comparar)){
+                        cuatro += 1;
+                        System.out.print(cuatro);
+                   }
+
+                    contador +=1;
+                    leer = leer + 5;
+                    //linea = "";
+                }
+
+                else{
+                    contador += 1;
+                }
+         }
         //LeerTxt llamando = new LeerTxt();
         //llamando.Horas("Regulares");
         
@@ -150,8 +160,8 @@ public class LeerTxt {
     }
     public static void main(String [] args){
     LeerTxt prueba = new LeerTxt();
-    prueba.Horas("Embarazadas");
-    prueba.Horas("Regulares");
+    //prueba.Horas("Embarazadas");
+    prueba.Horas();
     }
 }
 
