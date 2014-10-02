e55ee87230c14bf3321b11c1a6f5b771107b5272
chef19 <teckevin_16@hotmail.com>
2014-10-02 17:56:04 -0600

    Dato de Fecha

--- src/LeerTxt.java
+++ src/LeerTxt.java
@@ -1,7 +1,9 @@
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,9 +15,11 @@ import java.io.FileReader;
  * 
  */
 public class LeerTxt {
-      int contador;
+      int contador=0;
       String comparar;
-      int leer=3;
+      String comparar1;
+      int leerHora=4;
+      int leerFecha=5;
       int nueve=0;
       int diez=0;
       int once=0;
@@ -24,9 +28,41 @@ public class LeerTxt {
       int tres=0;
       int cuatro=0;
       int a;
+      int Fecha=0;
+      int d1=0;
+      int d2=0;
+      int d3=0;
+      int d4=0;
+      int d5=0;
+      int d6=0;
+      int d7=0;
+      int d8=0;
+      int d9=0;
+      int d10=0;
+      int d11=0;
+      int d12=0;
+      int d13=0;
+      int d14=0;
+      int d15=0;
+      int d16=0;
+      int d17=0;
+      int d18=0;
+      int d19=0;
+      int d20=0;
+      int d21=0;
+      int d22=0;
+      int d23=0;
+      int d24=0;
+      int d25=0;
+      int d26=0;
+      int d27=0;
+      int d28=0;
+      int d29=0;
+      int d30=0;
+      int d31=0;
+      String errorFecha;
       String categoria;
     public String Horas() {
-      String nl = System.getProperty("line.separator");
       File archivo = null;
       FileReader fr = null;
       BufferedReader br = null;
@@ -38,53 +74,52 @@ public class LeerTxt {
  
          // Lectura del fichero
          String linea;
-         contador =0;
          
-         for (a=0;a<2;a++){
+         for (a=0;a<5;a++){
              if (a==0){
                  categoria="Embarazadas";
              }
              if (a==1){
                  categoria="Regulares";
              }
+             if (a==2){
+                 categoria="Discapacitados";
+             }
+             if (a==3){
+                 categoria="Mayores";
+             }
+             if (a==4){
+                 categoria="Corporativos";
+             }
             archivo = new File ("Clientes/"+categoria+".txt");
             fr = new FileReader (archivo);
             br = new BufferedReader(fr);
             while((linea=br.readLine())!=null)
-                if (leer == contador){
+                if (leerHora == contador){
                     comparar = linea.substring(0, 2);
                     if ("09".equals(comparar)){
-                        nueve += 1;
-                        System.out.println("nueve  "+ nueve);
+                        nueve++;
                     }
                     if ("10".equals(comparar)){
-                        diez += 1;
-                        System.out.print(diez);
+                        diez++;
                    }
                     if ("11".equals(comparar)){
-                        once += 1;
-                        System.out.print(once);
+                        once++;
                    }
                     if ("01".equals(comparar)){
-                        una += 1;
-                        System.out.print(una);
+                        una++;
                    }
                     if ("02".equals(comparar)){
-                        dos += 1;
-                        System.out.println(dos);
+                        dos++;
                    }
                     if ("03".equals(comparar)){
-                        tres += 1;
-                        System.out.println("tres  "+tres);
+                        tres++;
                    }
                     if ("04".equals(comparar)){
-                        cuatro += 1;
-                        System.out.print(cuatro);
+                        cuatro++;
                    }
-
                     contador +=1;
-                    leer = leer + 5;
-                    //linea = "";
+                    leerHora = leerHora + 5;
                 }
 
                 else{
@@ -111,36 +146,154 @@ public class LeerTxt {
       }
         return null;
     }
-    public String Fecha() {
+    
+    
+   public String Fecha() throws FileNotFoundException, IOException {
       File archivo = null;
       FileReader fr = null;
       BufferedReader br = null;
-      int contador;
-      String variable = "";
-      String Fecha;
-      int leer=5;
-      int size = 0;
       
       try {
          // Apertura del fichero y creacion de BufferedReader para poder
          // hacer una lectura comoda (disponer del metodo readLine()).
-         archivo = new File ("Clientes/Embarazadas.txt");
-         fr = new FileReader (archivo);
-         br = new BufferedReader(fr);
- 
          // Lectura del fichero
          String linea;
-         contador =0;
-         while((linea=br.readLine())!=null)
-             if (leer == contador){
-                 linea = linea.substring(0, 2);
-                 System.out.print(linea);
-                 contador +=1;
-                 leer = leer + 6;
+         
+         for (a=0;a<5;a++){
+             if (a==0){
+                 categoria="Embarazadas";
              }
-             else{
-                 contador += 1;
+             if (a==1){
+                 categoria="Regulares";
              }
+             if (a==2){
+                 categoria="Discapacitados";
+             }
+             if (a==3){
+                 categoria="Mayores";
+             }
+             if (a==4){
+                 categoria="Corporativos";
+             }
+            archivo = new File ("Clientes/"+categoria+".txt");
+            fr = new FileReader (archivo);
+            br = new BufferedReader(fr);
+            while((linea=br.readLine())!=null){
+                if (leerFecha == contador){
+                    
+                    errorFecha = linea.substring(1, 2);
+                    System.out.println(errorFecha);
+                    //System.out.println(errorFecha);
+                    if (errorFecha=="/"){
+                        comparar = linea.substring(0, 1);
+                        
+                        if ("1".equals(comparar)){
+                            d1++;
+                        }
+                        if ("2".equals(comparar)){
+                            d2++;
+                            
+                        }
+                        if ("3".equals(comparar)){
+                            d3++;
+                        }
+                        if ("4".equals(comparar)){
+                            d4++;
+                        }
+                        if ("5".equals(comparar)){
+                            d5++;
+                        }
+                        if ("6".equals(comparar)){
+                            d6++;
+                        }
+                        if ("7".equals(comparar)){
+                            d7++;
+                        }
+                        if ("8".equals(comparar)){
+                            d8++;
+                        }
+                        if ("9".equals(comparar)){
+                            d9++;
+                        }
+                    }       
+                    else{
+                        comparar1=linea.substring(0, 2);
+                         if ("10".equals(comparar1)){
+                            d10++;
+                         }
+                         if ("11".equals(comparar1)){
+                            d11++;
+                         }
+                         if ("12".equals(comparar1)){
+                            d12++;
+                         }
+                         if ("13".equals(comparar1)){
+                            d13++;
+                         }
+                         if ("14".equals(comparar1)){
+                            d14++;
+                         }
+                         if ("15".equals(comparar1)){
+                            d15++;
+                         }
+                         if ("16".equals(comparar1)){
+                            d16++;
+                         }
+                         if ("17".equals(comparar1)){
+                            d17++;
+                         }
+                         if ("18".equals(comparar1)){
+                            d18++;
+                         }
+                         if ("19".equals(comparar1)){
+                            d19++;
+                         }
+                         if ("20".equals(comparar1)){
+                            d20++;
+                         }
+                         if ("21".equals(comparar1)){
+                            d21++;
+                         }
+                         if ("22".equals(comparar1)){
+                            d22++;
+                         }
+                         if ("23".equals(comparar1)){
+                            d23++;
+                         }
+                         if ("24".equals(comparar1)){
+                            d24++;
+                         }
+                         if ("25".equals(comparar1)){
+                            d25++;
+                         }
+                         if ("26".equals(comparar1)){
+                            d26++;
+                         }
+                         if ("27".equals(comparar1)){
+                            d27++;
+                         }
+                         if ("28".equals(comparar1)){
+                            d28++;
+                         }
+                         if ("29".equals(comparar1)){
+                            d29++;
+                         }
+                         if ("30".equals(comparar1)){
+                            d30++;
+                         }
+                         if ("31".equals(comparar1)){
+                            d31++;
+                         }
+                    }
+                }
+                else{
+                    contador += 1;
+                    leerFecha+=5;
+                    
+                }
+            }     
+         }
+         
       }
       catch(Exception e){
          e.printStackTrace();
@@ -153,15 +306,15 @@ public class LeerTxt {
                fr.close();     
             }                  
          }catch (Exception e2){ 
-           e2.printStackTrace();
+            e2.printStackTrace();
          }
       }
         return null;
     }
-    public static void main(String [] args){
+   
+    public static void main(String [] args) throws IOException{
     LeerTxt prueba = new LeerTxt();
-    //prueba.Horas("Embarazadas");
-    prueba.Horas();
+    prueba.Fecha();
     }
 }
 
