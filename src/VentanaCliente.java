5cc52b5340e7a9984ee57e3da06e8b13006e97ec
mell9413 <mell9413@hotmail.com>
2014-10-08 23:32:20 -0600

    Se corrigen algunos errores

--- src/VentanaCliente.java
+++ src/VentanaCliente.java
@@ -282,30 +282,30 @@ public class VentanaCliente extends javax.swing.JFrame {
         String nl = System.getProperty("line.separator");
         try {
             if (CheckClienteD.isSelected()==true) {
-                File archivo=new File("Clientes/Discapacitados.txt");
+                File archivo=new File("src/Clientes/Discapacitados.txt");
                 FileWriter escribir = new FileWriter(archivo,true);
                 escribir.write(++VentanaPrincipal.contadorDiscapacitados+nl+NombreCliente.getText()+nl+CorreoCliente.getText()+nl+"Discapacitados"+nl+jLabel1.getText()+nl+jLabel2.getText()+nl);
             }
             else if (CheckClienteM.isSelected()==true) {
-                File archivo=new File("Clientes/Mayores.txt");
+                File archivo=new File("src/Clientes/Mayores.txt");
                 FileWriter escribir = new FileWriter(archivo,true);
                 escribir.write(++VentanaPrincipal.contadorMayores+nl+NombreCliente.getText()+nl+CorreoCliente.getText()+nl+"Mayores"+nl+jLabel1.getText()+nl+jLabel2.getText()+nl);
                 
             }
             else if (CheckClienteE.isSelected()==true) {
-                File archivo=new File("Clientes/Embarazadas.txt");
+                File archivo=new File("src/Clientes/Embarazadas.txt");
                 FileWriter escribir = new FileWriter(archivo,true);
                 escribir.write(++VentanaPrincipal.contadorEmbarazadas+nl+NombreCliente.getText()+nl+CorreoCliente.getText()+nl+"Embarazadas"+nl+jLabel1.getText()+nl+jLabel2.getText()+nl);
                
             }
             else if (CheckClienteC.isSelected()==true) {
-                File archivo=new File("Clientes/Corporativos.txt");
+                File archivo=new File("src/Clientes/Corporativos.txt");
                 FileWriter escribir = new FileWriter(archivo,true);
                 escribir.write(++VentanaPrincipal.contadorCorporativos+nl+NombreCliente.getText()+nl+CorreoCliente.getText()+nl+"Coorporativos"+nl+jLabel1.getText()+nl+jLabel2.getText()+nl);
                 
             } 
             else if (CheckClienteR.isSelected()==true) {
-                File archivo=new File("Clientes/Regulares.txt");
+                File archivo=new File("src/Clientes/Regulares.txt");
                 FileWriter escribir = new FileWriter(archivo,true);
                 escribir.write(++VentanaPrincipal.contadorRegulares+nl+NombreCliente.getText()+nl+CorreoCliente.getText()+nl+"Regulares"+nl+jLabel1.getText()+nl+jLabel2.getText()+nl);
                 
