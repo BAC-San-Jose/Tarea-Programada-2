cc5ee626eeb959472f7e3d8faf948769ed39a3ca
chef19 <teckevin_16@hotmail.com>
2014-10-09 21:26:54 -0600

    Update VentanaCliente.java
    Errores con guardar

--- src/VentanaCliente.java
+++ src/VentanaCliente.java
@@ -286,26 +286,7 @@ public class VentanaCliente extends javax.swing.JFrame {
         else{
             try {
                 if (CheckClienteD.isSelected()==true) {
-                    /**try {
-                        String rutaGuardar="Clientes/texto.txt";
-                        //seleccionamos la ruta del archivo que vamos a guardar el archivo 
-                        File archivo = new File (rutaGuardar);
-                        if(!archivo.exists()){
-                        archivo.createNewFile();
-                        FileReader fr = new FileReader (archivo);
-                        String linea=++VentanaPrincipal.contadorDiscapacitados+nl+NombreCliente.getText()+nl+CorreoCliente.getText()+nl+"Discapacitados"+nl+jLabel1.getText()+nl+jLabel2.getText()+nl;
-                        BufferedWriter bfw=new BufferedWriter(new FileWriter(rutaGuardar));
-                        bfw.append(linea);
-                        bfw.flush();
-                        bfw.newLine();// crea una nueva linea
-                        }
-                    }catch(IOException e){ System.out.println(e.toString());
-                    }
-                    finally{
-                        archivo fr.
-                                }
-                                **/
-                    
+
                     try{
                     File archivo=new File(System.getProperty("user.dir")+"/Clientes/Discapacitados.txt");
                     FileWriter escribir = new FileWriter(archivo,true);
@@ -320,25 +301,25 @@ public class VentanaCliente extends javax.swing.JFrame {
                     File archivo=new File(System.getProperty("user.dir")+"/Clientes/Mayores.txt");
                     FileWriter escribir = new FileWriter(archivo,true);
                     escribir.write(++VentanaPrincipal.contadorMayores+nl+NombreCliente.getText()+nl+CorreoCliente.getText()+nl+"Mayores"+nl+jLabel1.getText()+nl+jLabel2.getText()+nl);
-
+                    escribir.close();
                 }
                 else if (CheckClienteE.isSelected()==true) {
                     File archivo=new File(System.getProperty("user.dir")+"/Clientes/Embarazadas.txt");
                     FileWriter escribir = new FileWriter(archivo,true);
                     escribir.write(++VentanaPrincipal.contadorEmbarazadas+nl+NombreCliente.getText()+nl+CorreoCliente.getText()+nl+"Embarazadas"+nl+jLabel1.getText()+nl+jLabel2.getText()+nl);
-
+                    escribir.close();
                 }
                 else if (CheckClienteC.isSelected()==true) {
                     File archivo=new File(System.getProperty("user.dir")+"/Clientes/Corporativos.txt");
                     FileWriter escribir = new FileWriter(archivo,true);
                     escribir.write(++VentanaPrincipal.contadorCorporativos+nl+NombreCliente.getText()+nl+CorreoCliente.getText()+nl+"Coorporativos"+nl+jLabel1.getText()+nl+jLabel2.getText()+nl);
-
+                    escribir.close();
                 } 
                 else if (CheckClienteR.isSelected()==true) {
                     File archivo=new File(System.getProperty("user.dir")+"/Clientes/Clientes/Regulares.txt");
                     FileWriter escribir = new FileWriter(archivo,true);
                     escribir.write(++VentanaPrincipal.contadorRegulares+nl+NombreCliente.getText()+nl+CorreoCliente.getText()+nl+"Regulares"+nl+jLabel1.getText()+nl+jLabel2.getText()+nl);
-
+                    escribir.close();
                 }
                 if(validateEmail(CorreoCliente.getText())==true){
                     Nombre = NombreCliente.getText();
